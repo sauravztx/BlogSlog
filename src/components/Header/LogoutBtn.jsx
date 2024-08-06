99c3858e7bb291a64718cb16719727942ab6f3a1
sauravztx <saurabh.ztx@gmail.com>
2024-08-06 15:25:26 +0530

    Added Demo Video

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -14,7 +14,7 @@ function LogoutBtn() {
     }
   return (
     <button
-    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    className='bg-red-500 text-white inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full hover:text-red-500'
     onClick={logoutHandler}
     >Logout</button>
   )
